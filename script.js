bc2785499d05b10aedd20d38a79851530bd736a2
ZetWu <63952346+zetwuu@users.noreply.github.com>
2025-12-04 15:36:44 +0000

    Détails et réel finalisation !

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const réponses = document.querySelector ('#juste');
-réponses.addEventListener('click', () => {
-    réponses.textContent = 'Bravo !'
-    
+const reponses = document.querySelector('#juste');
+reponses.addEventListener('click', () => {
+    reponses.textContent = 'Bravo !'
+
+});
+
+var resultat = localStorage.getItem('counter') | 0 ;
+document.querySelector('#juste');
+reponses.addEventListener('click', () => {
+    resultat++
+    localStorage.setItem('counter',resultat)
+    console.log(resultat)
+
+});
+ 
+
+function deleteItems() {
+    localStorage.clear();
+}
+
+const results = document.querySelector('#result');
+results.addEventListener('click', () => {
+    results.textContent = resultat
+
 });
